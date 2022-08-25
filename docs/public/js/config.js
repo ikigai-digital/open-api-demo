71b20a9377e61214a0180ff355ab7a8fac53ceec
Sohail Yar Khan <sohail@igikaidigital.io>
2022-08-25 16:20:03 +0800

    Add basic file structure

---
+++ docs/public/js/config.js
@@ -0,0 +1,12 @@
+const config = {
+  urls: [
+    {
+      url: 'http://localhost:3000/specs/petstoreV1.json',
+      name: 'PetstoreV1',
+    },
+    {
+      url: 'http://localhost:3000/specs/petstoreV2.json',
+      name: 'PetstoreV2',
+    },
+  ],
+}
