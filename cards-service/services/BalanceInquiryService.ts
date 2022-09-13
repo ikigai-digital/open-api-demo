274ea560fa4eae2cc36610e87b87394eadedc3c4
Sohail Yar Khan <sohail@igikaidigital.io>
2022-09-13 11:54:24 +0800

    Add wallet cloient

--- cards-service/services/BalanceInquiryService.ts
+++ cards-service/services/BalanceInquiryService.ts
@@ -1,8 +1,19 @@
 import { BaseService } from '../generated/provider/cards/cardsPostingApi/1.0.0'
+import { GetWalletsApi } from '../generated/client/wallet/getWalletsApi/1.0.0'
+
+const getWalletsApi = new GetWalletsApi()
 
 export const balanceAccount = (props) =>
   new Promise(async (resolve, reject) => {
     try {
+      try {
+        const res = await getWalletsApi.getWallets(1234)
+        console.log({ res })
+      } catch (error) {
+        console.log({ error })
+        console.log("API doesn't exist, continuing")
+      }
+
       resolve(
         BaseService.successResponse({
           ...props.body,
